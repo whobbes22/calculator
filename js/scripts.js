266729a4a030a898d6f8a62062d538840b279800
Joe W <whobbes22@gmail.com>
2023-03-20 11:43:44 -0700

    initalize project

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+
+//ui
+const num1 = parseInt(prompt("num1?"));
+const num2 = parseInt(prompt("num2?"));
+window.alert(add(num1,num2));
