a7af1fc9145819b6731ccc1810d13844369cc8e0
Joe W <whobbes22@gmail.com>
2023-03-20 16:47:26 -0700

    add the 4 basic operations

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,19 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
+function sub(num1,num2){
+  return num1 - num2;
+}
+function multi(num1,num2) {
+  return num1 * num2;
+}
+
+function divide(num1,num2){
+  return num1 / num2;
+}
+
 
 //ui
 const num1 = parseInt(prompt("num1?"));
 const num2 = parseInt(prompt("num2?"));
-window.alert(add(num1,num2));
+window.alert("adding numbers... " + add(num1,num2)+" -- Subtracting numbers... " + sub(num1,num2)+" -- Multiplying numbers... " + multi(num1,num2) +" -- Dividing numbers... " + divide(num1,num2));
